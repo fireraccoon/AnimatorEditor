5a883659bdaaf0053793ff854aa577071b9e79dc
fireraccoon <willyjames.music@gmail.com>
2015-07-17 01:38:41 -0400

    Arrow head good angle

--- graphicstransition.cpp
+++ graphicstransition.cpp
@@ -102,29 +102,29 @@ void GraphicsTransition::paint(QPainter *painter, const QStyleOptionGraphicsItem
     setLine(QLineF(intersectPoint, mCurrentState->getCenterPoint()));
 
     double angle = ::acos(line().dx() / line().length());
-    if (line().dy() >= 0){
+    if (line().dy() >= 0)
         angle = (PI * 2) - angle;
 
-        QPointF arrowP1 = line().p1() + QPointF(sin(angle + PI / 3) * arrowSize,
-                                        cos(angle + PI / 3) * arrowSize);
-        QPointF arrowP2 = line().p1() + QPointF(sin(angle + PI - PI / 3) * arrowSize,
-                                        cos(angle + PI - PI / 3) * arrowSize);
+    QPointF arrowP1 = line().p1() + QPointF(sin(angle + PI / 3) * arrowSize,
+                                    cos(angle + PI / 3) * arrowSize);
+    QPointF arrowP2 = line().p1() + QPointF(sin(angle + PI - PI / 3) * arrowSize,
+                                    cos(angle + PI - PI / 3) * arrowSize);
 
-        mArrowHead.clear();
-        mArrowHead << line().p1() << arrowP1 << arrowP2;
+    mArrowHead.clear();
+    mArrowHead << line().p1() << arrowP1 << arrowP2;
 
 
-        painter->drawLine(line());
-        painter->drawPolygon(mArrowHead);
-        if (isSelected()) {
-            painter->setPen(QPen(Qt::white, 1, Qt::DashLine));
+    painter->drawLine(line());
+    painter->drawPolygon(mArrowHead);
+    if (isSelected()) {
+        painter->setPen(QPen(Qt::white, 1, Qt::DashLine));
         QLineF myLine = line();
         myLine.translate(0, 2.0);
         painter->drawLine(myLine);
         myLine.translate(0,-4.0);
         painter->drawLine(myLine);
-        }
     }
+
 
     QGraphicsLineItem::paint(painter, option, widget);
 
